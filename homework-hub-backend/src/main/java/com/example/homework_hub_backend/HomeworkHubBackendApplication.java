5485754c10490fa4c19a4eee1953fa9b8722d3f7
pujajathar <pujajathar91@gmail.com>
2026-09-03 15:13:32 -0500

    Initial Commit

---
+++ homework-hub-backend/src/main/java/com/example/homework_hub_backend/HomeworkHubBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.homework_hub_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeworkHubBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeworkHubBackendApplication.class, args);
+	}
+
+}
---
+++ homework-hub-backend/src/test/java/com/example/homework_hub_backend/HomeworkHubBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.homework_hub_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeworkHubBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
