ca1bb8579bc8640f2c567ade02883b44ccc98f30
pujajathar <pujajathar91@gmail.com>
2026-09-15 15:22:42 -0500

    Added repository method to find attachments by assignment

--- homework-hub-backend/src/main/java/com/example/homework_hub_backend/repositories/AttachmentRepository.java
+++ homework-hub-backend/src/main/java/com/example/homework_hub_backend/repositories/AttachmentRepository.java
@@ -3,5 +3,8 @@ package com.example.homework_hub_backend.repositories;
 import com.example.homework_hub_backend.models.Attachment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface AttachmentRepository extends JpaRepository<Attachment, Long> {
+List<Attachment> findByAssignmentId(Long assignmentId); //finds attachments whose assignment id matches id we provide
 }
